de4ce1c682ebb396fb18ac5d5534febb9f985ea9
siddartha ps <vinayd098@gmail.com>
2023-12-27 19:43:03 +0530

    Created product filter and added more products, all the products are added in app.js in the form of products object. All the products are rendered inside products.html using javascript.

--- js/app.js
+++ js/app.js
@@ -1,252 +1,254 @@
-(function($) { 
-    $(function() { 
+(function ($) {
+  $(function () {
+    //  open and close nav
+    $("#navbar-toggle").click(function () {
+      $("nav ul").slideToggle();
+    });
 
-        // Open and close nav on hover
-        $('#navbar-toggle').hover(function() {
-            $('nav ul').slideToggle();
-        });
+    // Hamburger toggle
+    $("#navbar-toggle").on("click", function () {
+      this.classList.toggle("active");
+    });
 
-        // Hamburger toggle on hover
-        $('#navbar-toggle').on('mouseenter', function() {
-            this.classList.toggle('active');
-        });
+    // If a link has a dropdown, add sub menu toggle.
+    $("nav ul li a:not(:only-child)").click(function (e) {
+      $(this).siblings(".navbar-dropdown").slideToggle("slow");
 
-        // If a link has a dropdown, add sub menu toggle on hover
-        $('nav ul li').hover(
-            function() {
-                $(this).find('.navbar-dropdown').stop(true, true).slideDown("slow");
-            },
-            function() {
-                $(this).find('.navbar-dropdown').stop(true, true).slideUp("slow");
-            }
-        );
+      // Close dropdown when select another dropdown
+      $(".navbar-dropdown").not($(this).siblings()).hide("slow");
+      e.stopPropagation();
+    });
 
-        // Click outside the dropdown will remove the dropdown class
-        $('html').click(function() {
-            $('.navbar-dropdown').hide();
-        });
-
-        // Prevent the dropdown from closing when hovering over it
-        $('.navbar-dropdown').click(function(e) {
-            e.stopPropagation();
-        });
-    }); 
+    // Click outside the dropdown will remove the dropdown class
+    $("html").click(function () {
+      $(".navbar-dropdown").hide();
+    });
+  });
 })(jQuery);
-
 
 // Filter Products
 
- const products = {
-    allProducts: [
-        {
-  name: 'Body Wax Strips', 
-  image: './img/wax-strips.jpeg', 
-  description: 'Product Name: Body Wax Strips\nProduct Code: ZT-408\nPackaging: 16 PCS\nMRP: ₹289' },
-
-  {
-  name: 'Acne / Pimple Patch', 
-  image: './img/acne-pimple.jpeg', 
-  description: 'Product Name: Acne / Pimple Patch\nProduct Code: ZT-410\nPackaging: 36 Patches\nMRP: ₹279' },
-
-  {
-  name: 'Under Eye Pads', 
-  image: './img/eye-pads.jpeg', 
-  description: 'Product Name: UNder Eye Pads\nProduct Code: ZT-409\nPackaging: 3 PCS\nMRP: ₹279' },
-
-  {
-  name: 'Lip Mask', 
-  image: './img/lip-mask.jpeg', 
-  description: 'Product Name: Lip Mask\nProduct Code: ZT-411\nPackaging: 3 PCS\nMRP: ₹269' },
-
-  {
-  name: 'Cotton Buds Pouch 100s', 
-  image: './img/buds-75.jpeg', 
-  description: 'Product Name: Cotton Buds Pouch 100s\nProduct Code: ZT-401\nPackaging: 100 PCS\nMRP: ₹75' },
-
-//   {
-//   name: 'Cotton Buds Pouch 100s', 
-//   image: './img/buds-75.jpeg', 
-//   description: 'Product Name: Cotton Buds Pouch 100s\nProduct Code: ZT-402\nPackaging: 100 PCS\nMRP: ₹75' },
-
-  {
-  name: 'Cotton Buds Pouch 200s', 
-  image: './img/buds-129.jpeg', 
-  description: 'Product Name: Cotton Buds Pouch 200s\nProduct Code: ZT-403\nPackaging: 200 PCS\nMRP: ₹129' },
-
-  {
-  name: 'Cotton Buds Pouch 300s', 
-  image: './img/buds-199.jpeg', 
-  description: 'Product Name: Cotton Buds Pouch 300s\nProduct Code: ZT-404\nPackaging: 300 PCS\nMRP: ₹199' },
-],
-
-
-
-skinCare: [
+const products = {
+  allProducts: [
     {
-        name: 'Body Wax Strips', 
-        image: './img/wax-strips.jpeg', 
-        description: 'Product Name: Body Wax Strips\nProduct Code: ZT-408\nPackaging: 16 PCS\nMRP: ₹289' },
+      id:1,
+      name: "Body Wax Strips",
+      image: "./img/wax-strips.jpeg",
+      description:
+        " \nProduct Code: ZT-408\nPackaging: 16 PCS\nMRP: ₹289",
+      category: "skinCare",
+    },
 
-  {
-    name: 'Acne / Pimple Patch', 
-    image: './img/acne-pimple.jpeg', 
-    description: 'Product Name: Acne / Pimple Patch\nProduct Code: ZT-410\nPackaging: 36 Patches\nMRP: ₹279' },
-],
-
-
-
-
-faceCare: [
     {
-        name: 'Under Eye Pads', 
-        image: './img/eye-pads.jpeg', 
-        description: 'Product Name: UNder Eye Pads\nProduct Code: ZT-409\nPackaging: 3 PCS\nMRP: ₹279' },
+      id:2,
+      name: "Acne / Pimple Patch",
+      image: "./img/acne-pimple.jpeg",
+      description:
+        " \nProduct Code: ZT-410\nPackaging: 36 Patches\nMRP: ₹279",
+        category: "skinCare",
+    },
 
-  {
-        name: 'Lip Mask', 
-        image: './img/lip-mask.jpeg', 
-        description: 'Product Name: Lip Mask\nProduct Code: ZT-411\nPackaging: 3 PCS\nMRP: ₹269' },
-],
-
-
-
-generalCare: [
-// {
-//   name: 'Cotton Buds Pouch 100s', 
-//   image: '/cool-and-cool/skin care products/General care-1.jpg', 
-//   description: 'Product Name: Cotton Buds Pouch 100s\nProduct Code: ZT-401\nPackaging: 100 PCS\nMRP: $0.60' },
-
-{
-    name: 'Cotton Buds Pouch 100s', 
-    image: './img/buds-75.jpeg', 
-    description: 'Product Name: Cotton Buds Pouch 100s\nProduct Code: ZT-401\nPackaging: 100 PCS\nMRP: ₹75' },
-  
     {
-    name: 'Cotton Buds Pouch 200s', 
-    image: './img/buds-129.jpeg', 
-    description: 'Product Name: Cotton Buds Pouch 200s\nProduct Code: ZT-403\nPackaging: 200 PCS\nMRP: ₹129' },
-  
+      id:3,
+      name: "Under Eye Pads",
+      image: "./img/eye-pads.jpeg",
+      description:
+        " UNder Eye Pads\nProduct Code: ZT-409\nPackaging: 3 PCS\nMRP: ₹279",
+        category: "faceCare"
+    },
+
     {
-    name: 'Cotton Buds Pouch 300s', 
-    image: './img/buds-199.jpeg', 
-    description: 'Product Name: Cotton Buds Pouch 300s\nProduct Code: ZT-404\nPackaging: 300 PCS\nMRP: ₹199' },
-]
+      id:4,
+      name: "Lip Mask",
+      image: "./img/lip-mask.jpeg",
+      description:
+        " \nProduct Code: ZT-411\nPackaging: 3 PCS\nMRP: ₹269",
+        category: "faceCare"
+    },
+
+    {
+      id:5,
+      name: "Cotton Buds Pouch 100s",
+      image: "./img/buds-75.jpeg",
+      description:
+        "\nProduct Code: ZT-401\nPackaging: 100 PCS\nMRP: ₹75",
+        category: "generalCare"
+    },
+
+    {
+      id:6,
+      name: "Cotton Buds Pouch 200s",
+      image: "./img/buds-129.jpeg",
+      description:
+        "\nProduct Code: ZT-403\nPackaging: 200 PCS\nMRP: ₹129",
+        category: "generalCare"
+    },
+
+    {
+      id:7,
+      name: "Cotton Buds Pouch 300s",
+      image: "./img/buds-199.jpeg",
+      description:
+        " \nProduct Code: ZT-404\nPackaging: 300 PCS\nMRP: ₹199",
+        category: "generalCare"
+    },
+
+    {
+      id:8,
+      name: "Watermelon Nose Stripes",
+      image: "./img/watermelon-nose-stripes.png",
+      description:
+        "\nProduct Code: \nPackaging: \nMRP: ",
+        category: "faceCare"
+    },
+    {
+      id:9,
+      name: "Luxary Bath Loofah",
+      image: "./img/luxary-bath-loofah.png",
+      description:
+        " \nProduct Code: \nPackaging: \nMRP: ",
+        category: "generalCare"
+    },
+    {
+      id:10,
+      name: "Premium Delux Napkins",
+      image: "./img/premium-delux-napkins.png",
+      description:
+        " \nProduct Code: \nPackaging: \nMRP: ",
+        category: "generalCare"
+    },
+    {
+      id:11,
+      name: "Coffee Nose Stripes",
+      image: "./img/coffee-nose-stripes.png",
+      description:
+        "\nProduct Code: \nPackaging: \nMRP: ",
+        category: "faceCare"
+    },
+  ],
 };
 
-// function showProducts(category) {
-// const productContainer = document.getElementById('product-container');
-// // Clear previous content
-// productContainer.innerHTML = '';
-// productContainer.classList.add=("") 
 
-// const categoryProducts = products[category];
-
-// if (categoryProducts) {
-// categoryProducts.forEach(product => {
-//   const productCard = document.createElement('div');
-//   productCard.className = 'product-card';
-
-//   const img = document.createElement('img');
-//   img.src = product.image;
-//   img.alt = product.name;
-
-//   const productName = document.createElement('h3');
-//   productName.textContent = product.name;
-
-//   const productDescription = document.createElement('div');
-//   productDescription.className = 'product-description';
-//   productDescription.classList.add("container","text-center")
-
-//   // Create a table for additional information
-//   const table = document.createElement('table');
-
-//   // Split the description into lines
-//   const descriptionLines = product.description.split('\n');
-
-//   // Create a table row for each line
-//   descriptionLines.forEach(line => {
-//     const row = document.createElement('tr');
-//     const cell = document.createElement('td');
-//     cell.textContent = line;
-//     row.appendChild(cell);
-//     table.appendChild(row);
-//   });
-
-//   productDescription.appendChild(table);
-
-//   productCard.appendChild(img);
-//   productCard.appendChild(productName);
-//   productCard.appendChild(productDescription);
-
-//   productContainer.appendChild(productCard);
-// });
-// }
-// }
 
 function showProducts(category) {
-    const productContainer = document.getElementById('product-container');
-    productContainer.innerHTML = ''; // Clear previous content
-  
-    const categoryProducts = products[category];
-  
-    if (categoryProducts) {
-      categoryProducts.forEach(product => {
-        const productCard = document.createElement('div');
-        productCard.className = 'product-card';
-  
-        const img = document.createElement('img');
-        img.src = product.image;
-        img.alt = product.name;
-        img.onclick = function() {
-          openZoomModal(product.image);
-        };
-  
-        const productName = document.createElement('h3');
-        productName.textContent = product.name;
-  
-        const productDescription = document.createElement('div');
-        productDescription.className = 'product-description';
-        productDescription.classList.add("text-center", "w-100");
-  
-        // Create a table for additional information
-        const table = document.createElement('table');
-  
-        // Split the description into lines
-        const descriptionLines = product.description.split('\n');
-  
-        // Create a table row for each line
-        descriptionLines.forEach(line => {
-          const row = document.createElement('tr');
-          const cell = document.createElement('td');
-          cell.textContent = line;
-          row.appendChild(cell);
-          table.appendChild(row);
-        });
-  
-        productDescription.appendChild(table);
-  
-        productCard.appendChild(img);
-        productCard.appendChild(productName);
-        productCard.appendChild(productDescription);
-  
-        productContainer.appendChild(productCard);
-      });
-    }
-  }
-  
-  
-  // Function to show the zoom modal
-  function openZoomModal(imageSrc) {
-        const modal = document.getElementById('zoom-modal');
-        const modalImg = document.getElementById('modal-img');
-  
-        modal.style.display = 'block';
-        modalImg.src = imageSrc;
-      }
-  
-      function closeZoomModal() {
-        const modal = document.getElementById('zoom-modal');
-        modal.style.display = 'none';
-      }
-  
+  const productContainer = document.getElementById("product-container");
+  productContainer.innerHTML = ""; // Clear previous content
 
+  const filteredProducts = products.allProducts.filter(
+    (product) => product.category === category
+  );
+
+  if (filteredProducts.length > 0) {
+    filteredProducts.forEach((product) => {
+      const productCard = document.createElement("div");
+      productCard.className = "product-card";
+
+      const img = document.createElement("img");
+      img.src = product.image;
+      img.alt = product.name;
+      img.onclick = function () {
+        openZoomModal(product.image);
+      };
+
+      const productName = document.createElement("h3");
+      productName.textContent = product.name;
+
+      const productDescription = document.createElement("div");
+      productDescription.className = "product-description";
+      productDescription.classList.add("text-center", "w-100");
+
+      // Create a table for additional information
+      const table = document.createElement("table");
+
+      // Split the description into lines
+      const descriptionLines = product.description.split("\n");
+
+      // Create a table row for each line
+      descriptionLines.forEach((line) => {
+        const row = document.createElement("tr");
+        const cell = document.createElement("td");
+        cell.textContent = line;
+        row.appendChild(cell);
+        table.appendChild(row);
+      });
+
+      productDescription.appendChild(table);
+
+      productCard.appendChild(img);
+      productCard.appendChild(productName);
+      productCard.appendChild(productDescription);
+
+      productContainer.appendChild(productCard);
+    });
+  }
+}
+
+
+
+function displayAllProducts() {
+  const productContainer = document.getElementById("product-container");
+
+  // Clear previous content
+  productContainer.innerHTML = "";
+
+  products.allProducts.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.className = "product-card";
+
+    const img = document.createElement("img");
+    img.classList.add("mb-3","object-fit-cover")
+    img.src = product.image;
+    img.alt = product.name;
+    img.onclick = function () {
+      openZoomModal(product.image);
+    };
+
+    const productName = document.createElement("h2");
+
+    productName.textContent = product.name;
+
+    const productDescription = document.createElement("div");
+    productDescription.className = "product-description";
+    productDescription.classList.add("text-center","m-auto","p-0");
+
+    // Create a table for additional information
+    const table = document.createElement("table");
+
+    // Split the description into lines
+    const descriptionLines = product.description.split("\n");
+
+    // Create a table row for each line
+    descriptionLines.forEach((line) => {
+      const row = document.createElement("tr");
+      const cell = document.createElement("td");
+      cell.textContent = line;
+      row.appendChild(cell);
+      table.appendChild(row);
+    });
+
+    productDescription.appendChild(table);
+
+    productCard.appendChild(img);
+    productCard.appendChild(productName);
+    productCard.appendChild(productDescription);
+
+    productContainer.appendChild(productCard);
+  });
+}
+
+
+window.addEventListener("load", displayAllProducts);
+
+// Function to show the zoom modal
+function openZoomModal(imageSrc) {
+  const modal = document.getElementById("zoom-modal");
+  const modalImg = document.getElementById("modal-img");
+
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+}
+
+function closeZoomModal() {
+  const modal = document.getElementById("zoom-modal");
+  modal.style.display = "none";
+}
